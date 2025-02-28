60f1f1cd4bdfaf82ab7c603117502d326d4e5071
Yahiron <j0821305@yahoo.co.jp>
2025-02-28 23:41:54 +0900

    kadai_015の課題修正

--- src/kadai_015/CarExec_Chapter15.java
+++ src/kadai_015/CarExec_Chapter15.java
@@ -4,7 +4,7 @@ public class CarExec_Chapter15 {
 
 	 public static void main(String[] args) {
 	
-Car_Chapter15 car = new Car_Chapter15(1,10);
+		 Car_Chapter15 car = new Car_Chapter15();
 	
 	
 car.gearChange(3);//（）で指定したaftergearの結果の実行
--- src/kadai_015/Car_Chapter15.java
+++ src/kadai_015/Car_Chapter15.java
@@ -1,21 +1,28 @@
 package kadai_015;
 
 public class Car_Chapter15 {
-
+	//クラス
+	
+	
   private int gear  =1; //1速から5速のギアを表す 変数　初期値　1
   private int  speed =10;  //ギアチェンジ後の速度を表す　変数　初期値　10
-
+    //フィールド
   
-  public Car_Chapter15(int gear, int speed) { 
-	  //Car_Chapter15のgear変数とspeed変数をアクセスできるようにする。(=メゾット)
-		this.gear = gear;
-		this.speed = speed;
+  public Car_Chapter15() { 
+	  //CarExec_Chapter15がアクセスできるようにする。（メゾット）
+	
   
   }
   
- public void gearChange(int afterGear) {     //ギアの値により速度を変える
-	 	 
-	speed=switch(afterGear) {
+  //ここまでが車クラスのインスタンス
+  
+ public void gearChange(int afterGear) {     //ギアの値により速度を変えるメゾット
+	 
+	 int beforeGear=this.gear;	 
+	 this.gear = afterGear;
+	 
+	 
+	 this.speed=switch(afterGear) {
     case 1  -> 10;   //1速＝10ｋｍ
     case 2  -> 20;   //2速＝20ｋｍ
     case 3  -> 30;   //3速＝30ｋｍ
@@ -26,18 +33,15 @@ public class Car_Chapter15 {
 	 };
 	 
 	 
-	 System.out.println("ギア"+gear+"から"+afterGear+"に切り替えました"); 
+	 System.out.println("ギア"+beforeGear+"から"+afterGear+"に切り替えました"); 
 	//結果の表示
  }
 
-	 public void run() {      //ギアチェンジ後の速度を表示する
+	 public void run() {      //ギアチェンジ後の速度を表示するメゾット
 	
 	 System.out.println("速度は時速" + speed +"kmです");
 	//結果の表示
 	}	
-	
-	
-	 
 	
 	}
 
